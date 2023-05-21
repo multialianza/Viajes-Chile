426d61d7a584f55431ea0ad583c99e042192b34f
ivan <ivansalazarzepeda@gmail.com>
2023-05-21 00:34:42 -0400

    actualizacion archivos

---
+++ assets/js/viajeschile5.js
@@ -0,0 +1,22 @@
+$(function(){
+
+    $("a").click(function(event){
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            var suave = this.hash; 
+
+            $("html, body"). animate({
+                scrollTop: $(suave).offset().top
+            }, 1000, function(){
+                window.location.hash = suave;
+            }) ;
+        }
+    });
+
+});
+
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
